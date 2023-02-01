570423da2eebffc84a1d19d13c547fb0140292ae
David Sha <david.sha2002@gmail.com>
2023-02-02 07:38:31 +1100

    better summarization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 from os.path import join, dirname, realpath
 from tempfile import TemporaryDirectory
+import re
 
 import openai
 import PyPDF2
@@ -11,7 +12,6 @@ import pytesseract
 
 # OpenAI API key path
 OPENAI_API_KEY_PATH = "private/openai_api_key.txt"
-
 
 # Folder target containing the pdfs
 FOLDER = "samples"
@@ -25,8 +25,14 @@ DEBUG = True
 # Completion length
 COMPLETION_LENGTH = 100
 
+# Summarization length
+SUMMARIZATION_LENGTH = 300
+
 # Maximum content length
 MAX_CONTENT_LENGTH = 4097
+
+# Chunk size
+CHUNK_SIZE = MAX_CONTENT_LENGTH - SUMMARIZATION_LENGTH
 
 def send_to_file(f, data):
     '''
@@ -70,7 +76,7 @@ def pdf_to_text_ocr(pdf_path):
             text += pytesseract.image_to_string(image)
     return text
 
-def chunkify(text, chunk_size=MAX_CONTENT_LENGTH):
+def chunkify(text, chunk_size=CHUNK_SIZE):
     '''
     Split a text into chunks of size `chunk_size`. The split is done on a new line.
     '''
@@ -90,11 +96,14 @@ def openai_summarize_helper(text):
         model="text-davinci-003",
         prompt=f"Summarize the following text ensuring that details are still specific:\n```\n{text}\n```",
         temperature=0.7,
-        max_tokens=256,
+        max_tokens=SUMMARIZATION_LENGTH,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
+
+    if response.get("choices")[0].get("finish_reason") == "length":
+        print("Summarization was cut short. Consider increasing the `SUMMARIZATION_LENGTH` variable. Continuing...")
 
     if DEBUG:
         print(json.dumps(response, indent=4))
@@ -106,7 +115,7 @@ def openai_summarize(text):
     Summarize a given text using openai. If the text is too long, split it
     into smaller chunks and summarize each chunk.
     '''
-    if len(text) > MAX_CONTENT_LENGTH:
+    if len(text) > CHUNK_SIZE:
         
         # split the text into chunks
         chunks = chunkify(text)
@@ -125,12 +134,13 @@ def openai_generate(text, filename):
     '''
     # summarize in order to reduce the size of the text
     while len(text) + COMPLETION_LENGTH > MAX_CONTENT_LENGTH:
+        print("Summarizing text...")
         text = openai_summarize(text)
 
     # make the request
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"Return in the json format with the following fields extracted from the input text:\n- date\n- client_name\n- location\n\n\nAdditionally, add a confidence score between 0 and 1 to the same payload that indicates how certain you are about the correctness of the details.\n\n\nOnly answer with a json and nothing more.\n\n\nIf you couldn't find certain details, mark them as `null`.\n\n\nNormalise the date to YYYY-mm-dd.\n\n\nNormalise the location to \"number street, city, state, postcode\"\n\n\nHere is the input text: \n```\n{text}\n```\n\n\nHere is also the input text's filename: \n```\n{filename}\n```\n",
+        prompt=f"Return in json format with the following fields extracted from the input text:\n- date\n- client_name\n- location\n\n\nAdditionally, add a confidence score between 0 and 1 to the same payload that indicates how certain you are about the correctness of the details.\n\n\nIf you couldn't find certain details, make them `null`.\n\n\nNormalise the date to YYYY-mm-dd.\n\n\nNormalise the location to \"number street, city, state, postcode\"\n\n\nOnly reply with json and nothing more.\n\n\nHere is the input text: \n```\n{text}\n```\n\n\nHere is also the input text's filename: \n```\n{filename}\n```\n",
         temperature=0.7,
         max_tokens=COMPLETION_LENGTH,
         top_p=1,
@@ -138,10 +148,41 @@ def openai_generate(text, filename):
         presence_penalty=0
     )
 
+    if response.get("choices")[0].get("finish_reason") == "length":
+        print("Completion was cut short. Consider increasing the `COMPLETION_LENGTH` variable. Continuing...")
+
     if DEBUG:
         print(json.dumps(response, indent=4))
 
     return response.get("choices")[0].get("text")
+
+def extract(f):
+    '''
+    Extract text from a pdf file and send it to openai to generate a response.
+    '''
+    # extract text from pdf
+    text = pdf_to_text_ocr(join(dirname(realpath(__file__)), FOLDER, f))
+    if DEBUG:
+        print(text)
+    
+    # send the text to a file that has the same name as the pdf but 
+    # with .txt extension to the same folder
+    send_to_file(join(dirname(realpath(__file__)), FOLDER, "".join(f.split(".")[:-1]) + ".txt"), text)
+
+    # use openai to generate a response
+    try:
+        response = openai_generate(text, f)
+    except Exception as e:
+        print(e)
+        response = ""
+    
+    # print the response
+    try:
+        print(json.dumps(json.loads(response), indent=4))
+    except:
+        print(response)
+
+    return response
 
 def main(): 
 
@@ -156,27 +197,18 @@ def main():
         if not f.endswith(".pdf"):
             continue
 
-        # extract text from pdf
-        text = pdf_to_text_ocr(join(dirname(realpath(__file__)), FOLDER, f))
-        if DEBUG:
-            print(text)
-        
-        # send the text to a file that has the same name as the pdf but 
-        # with .txt extension to the same folder
-        send_to_file(join(dirname(realpath(__file__)), FOLDER, "".join(f.split(".")[:-1]) + ".txt"), text)
+        # extract text and generate a response
+        response = extract(f)
 
-        # use openai to generate a response
+        # match the json from the string of text,
+        # the json is surrounded by curly braces
         try:
-            response = openai_generate(text, f)
-        except Exception as e:
-            print(e)
-            response = ""
-        
-        # print the response
-        try:
-            print(json.dumps(json.loads(response), indent=4))
+            # remove new lines so that the regex works
+            response = response.replace("\n", "")
+
+            response = re.search(r"{.*}", response).group(0)
         except:
-            print(response)
+            pass
 
         # add the response to the list of responses
         try:
